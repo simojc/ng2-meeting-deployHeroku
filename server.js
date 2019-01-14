b3be2495627900516824b6bc9cb95580f923ff6c
simocl2017 <simocl@yahoo.fr>
2019-01-14 18:01:32 -0500

    commit deploiement Heroku lundi soir

--- server.js
+++ server.js
@@ -4,10 +4,17 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/ng2-meeting'));
+/* app.use(express.static(__dirname + '/dist/ng2-meeting'));
 
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+'/dist/ng2-meeting/index.html'));
+});
+ */
+
+// Serve only the static files from the dist directory
+app.use(express.static(__dirname + '/dist'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
