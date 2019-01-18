87f48b729760d15033953577a3e714636e2e359e
simocl2017 <simocl@yahoo.fr>
2019-01-17 20:14:06 -0500

    commit maj ghomala et change app name on  Heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   // API_URL: 'http://localhost/~simojc/phpapi/public/api/'
   // Redefinir cette variable (API_URL) avant de déployer
-  API_URL_NODEJS: 'https://gestassobk.herokuapp.com',
+  API_URL_NODEJS: 'https://gestassobk.herokuapp.com/',
 };
