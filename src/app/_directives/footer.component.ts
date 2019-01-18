e801eb98a385e0e6021b5e011e5a7fb3a5be9d0c
simocl2017 <simocl@yahoo.fr>
2019-01-17 19:45:47 -0500

    commit maj ghomala et change app name on  Heroku

--- src/app/_directives/footer.component.ts
+++ src/app/_directives/footer.component.ts
@@ -1,21 +1,20 @@
 import { Component, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs/Subscription';
- 
+
 import { AlertService } from '../_services/index';
- 
+
 @Component({
     selector: 'footer',
     template: `
-     
     <div style="text-align: center;" class="row">
     <hr />
       <span >
-        &copy; Copyright 2018, Fermat Soft. Tous droits réservés.
+        &copy; Copyright 2018, SimSoft. Tous droits réservés.
       </span>
    </div>
   `,
 })
- 
+
 export class FooterComponent  {
 
 }
